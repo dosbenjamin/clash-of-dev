4ae645fbdf9022dfa792161808cbb9ebf50f7d4e
Benjamin Dos Santos <hello@benjamindossantos.be>
2024-09-19 00:18:54 +0200

    feat: add page header section

--- src/scripts/lenis.ts
+++ src/scripts/lenis.ts
@@ -1,10 +1,4 @@
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import 'lenis/dist/lenis.css';
 import Lenis from 'lenis';
 
 export const lenis = new Lenis();
-
-lenis.on('scroll', ScrollTrigger.update);
-gsap.ticker.add((time) => lenis.raf(time * 1000));
-gsap.ticker.lagSmoothing(0);
